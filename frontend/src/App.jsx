bbf8959f94c7defe111af656959204e1e3b315ee
Pritam Neog <neog.geek123@gmail.com>
2024-08-15 21:32:17 +0530

    better folder structure - created frontend and backend directories

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { KanbanBoard } from "./components/CustomKanban";
+
+const App = () => {
+  return (
+    <div className="flex justify-center bg-neutral-900">
+      <div>
+        <KanbanBoard />
+      </div>
+    </div>
+  );
+};
+
+export default App;
